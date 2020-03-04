e496c41524563e48bca15069a87faa81efe3d30b
775788531@qq.com <LINhuaiyou.4013>
2020-03-04 12:32:10 +0800

    update boot 2.0.2 cloud finchley

--- src/main/java/com/hayes/code/cloudekserver/CloudEkServerApplication.java
+++ src/main/java/com/hayes/code/cloudekserver/CloudEkServerApplication.java
@@ -3,6 +3,10 @@ package com.hayes.code.cloudekserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +16,13 @@ public class CloudEkServerApplication {
         SpringApplication.run(CloudEkServerApplication.class, args);
     }
 
+    @EnableWebSecurity
+    @Configuration
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable(); //关闭csrf
+            http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); //开启认证
+        }
+    }
 }
